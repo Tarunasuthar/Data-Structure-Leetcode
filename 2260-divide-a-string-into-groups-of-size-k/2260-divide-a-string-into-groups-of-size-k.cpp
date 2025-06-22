17ea2cb5f22946c28566f1fb8384360d3f0344b0
Tarunasuthar <120656706+Tarunasuthar@users.noreply.github.com>
2025-06-22 11:49:44 +0530

    Time: 1 ms (22.11%), Space: 9.7 MB (70.3%) - LeetHub

--- 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.cpp
+++ 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.cpp
@@ -6,15 +6,8 @@ public:
         }
         int size = s.size();
         vector<string> res;
-        string t2;
-        int i=1;
-        while(i<=size){
-            t2 += s[i-1];
-            if(i%k==0){
-               res.push_back(t2);
-               t2 = "";
-            }
-            i++;
+        for(int i=0;i<size;i+=k){
+            res.push_back(s.substr(i,k));
         }
         return res;
     }

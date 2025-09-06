c1cff1ac724e59225d148517dccc448889217d1b
Tarunasuthar <120656706+Tarunasuthar@users.noreply.github.com>
2025-09-06 12:10:03 +0530

    Time: 0 ms (100%), Space: 43.8 MB (5.22%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -6,14 +6,14 @@ public:
         vector<int> pre(size,0);
         vector<int> suf(size,0);
         pre[0] = 1;
-        pre[1] = nums[0];
-        for(int i=2;i<size;i++){
+        // pre[1] = nums[0];
+        for(int i=1;i<size;i++){
             pre[i] = nums[i-1]*pre[i-1];
         }
         // p = 1;
         suf[size-1] = 1;
-        suf[size-2] = nums[size-1];
-        for(int j=size-3;j>=0;j--){
+        // suf[size-2] = nums[size-1];
+        for(int j=size-2;j>=0;j--){
             suf[j] = nums[j+1]*suf[j+1];
         }
         vector<int> ans;

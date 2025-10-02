900bfc9cc9ddde449577bbbd2d8e625a148c35e2
Tarunasuthar <120656706+Tarunasuthar@users.noreply.github.com>
2025-10-02 12:59:35 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.88%) - LeetHub

--- 3336-water-bottles-ii/3336-water-bottles-ii.cpp
+++ 3336-water-bottles-ii/3336-water-bottles-ii.cpp
@@ -3,7 +3,6 @@ public:
     int maxBottlesDrunk(int numBottles, int numExchange) {
           int full = 0;
           int a = numBottles;
-          int count = 0;
           while(numBottles>=numExchange){
             numBottles = numBottles-numExchange;
             numExchange++;
